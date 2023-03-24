de7d21890e74192f6992af26b0d4ab6b141b65c8
Lluvia <friasllu@gmail.com>
2023-03-24 15:56:40 -0400

    commit tree 10fd7d09cc08575e3841bb96c160612d1116c775

--- lib/my_all.rb
+++ lib/my_all.rb
@@ -1,5 +1,23 @@
 require 'pry'
 
 def my_all?(collection)
+  
+  i=0 #step 1a counter needed since we're using 'while'(lower-level iterator)
+  block_return_values = [] #step 4 to store in the new values we declare an empty array BEFORE entereing 'while' loop
+  
+  while i < collection.length
+    block_return_values << yield(collection[i]) #step 2 'yield' will send whatever is passed in as the argument to the block; 
+    #step 5 shovel in '<<' the return value of the block into new array
+  i += 1#step 1b explicit incrementation needed bc of 'while'
+  end
+
+  if block_return_values.include?(false) #step 6 use '.include?' to determine return values of my_all? method
+    false
+  else 
+    true
+  end
 
 end
+
+#call to method: step 3
+my_all?([1,2,3]) {|i| i<2}
